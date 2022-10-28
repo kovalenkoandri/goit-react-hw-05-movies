abddda37488bfef87c4e73845864f21aba4e41e0
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-10-29 01:48:20 +0300

    /

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -73,7 +73,7 @@ export const App = () => {
             }
           />
           <Route
-            path="/movies/"
+            path="/movies"
             element={
               <Movies
                 {...{
