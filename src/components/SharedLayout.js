970e9b22d13d18bc0e9e8c45e0f5c5754117a062
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-10-29 05:46:37 +0300

    prettier SharedLayout

--- src/components/SharedLayout.js
+++ src/components/SharedLayout.js
@@ -1,17 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import { Container, Header, Link } from 'components/App/App.styled';
-import {  Suspense } from 'react';
+import { Suspense } from 'react';
 const SharedLayout = () => {
   return (
     <Container>
-        <Header>
-          <nav>
-            <Link to="/" end>
-              Home
-            </Link>
-            <Link to="/movies">Movies</Link>
-          </nav>
-        </Header>
+      <Header>
+        <nav>
+          <Link to="/" end>
+            Home
+          </Link>
+          <Link to="/movies">Movies</Link>
+        </nav>
+      </Header>
       <Suspense fallback={<div>Loading...</div>}>
         <Outlet />
       </Suspense>
